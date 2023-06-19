cdedfd6bc02e78e633bc16648ba8a94d64c8b816
leonardosette <126572265+leonardosette@users.noreply.github.com>
2023-06-19 15:35:51 -0300

    Update q3.go

--- q3.go
+++ q3.go
@@ -1,78 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Participant struct {
-	Name string
-	Role string
-}
+//Crie uma struct chamada Triângulo com os campos "base" e
+//"altura". Escreva uma função que receba um Triângulo como
+//parâmetro e calcule a área do triângulo (área = base * altura / 2).
 
-func calculateMaxTeams(participants []Participant) (int, error) {
-	programmers := 0
-	mathematicians := 0
-	
-	for _, participant := range participants {
-		if participant.Role == "Programmer" {
-			programmers++
-		} else if participant.Role == "Mathematician" {
-			mathematicians++
-		}
-	}
-
-	for _, participant := range participants {
-		if participant.Name == "Pedro" || participant.Name == "Vanessa" {
-			if programmers < 1 {
-				return 0, fmt.Errorf("Não há programadores suficientes")
-			}
-		} else if participant.Name == "Tônia" || participant.Name == "João" {
-			if mathematicians < 1 {
-				return 0, fmt.Errorf("Não há matemáticos suficientes")
-			}
-		}
-	}
-
-	maxTeams := 0
-	if programmers >= 1 && mathematicians >= 1 {
-		minTeamMembers := min(programmers/4, mathematicians/4)
-		maxTeams = minTeamMembers
-	}
-
-	return maxTeams, nil
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+type Triangulo struct {
+	base   float64
+	altura float64
+	area   float64
 }
 
 func main() {
-	participants := []Participant{
-		{
-			Name: "Pedro",
-			Role: "Programmer",
-		},
-		{
-			Name: "Vanessa",
-			Role: "Programmer",
-		},
-		{
-			Name: "Tônia",
-			Role: "Mathematician",
-		},
-		{
-			Name: "João",
-			Role: "Mathematician",
-		},
-	}
-
-	maxTeams, err := calculateMaxTeams(participants)
-	if err != nil {
-		fmt.Println("Erro:", err)
-	} else {
-		fmt.Println("Número máximo de equipes:", maxTeams)
-	}
+	p := Triangulo{base: 10, altura: 5}
+	fmt.Println(areaTriangulo(p))
+}
+func areaTriangulo(p Triangulo) (area float64) {
+	area = (p.base * p.altura) / 2
+	return area
 }
